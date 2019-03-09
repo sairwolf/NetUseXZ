65596c1ac0d4d8e061abbac421e17d45784f7134
sairwolf <sairwolf@gamil.com>
2019-03-09 15:02:05 +0800

    v1.0
    第一版

---
+++ Form1.Designer.vb
@@ -0,0 +1,171 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請勿使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.txt_user = New System.Windows.Forms.TextBox()
+        Me.txt_pwd = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.lbl_user = New System.Windows.Forms.Label()
+        Me.lbl_pwd = New System.Windows.Forms.Label()
+        Me.lbl_server = New System.Windows.Forms.Label()
+        Me.lbl_ip = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lbl_title = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txt_user
+        '
+        Me.txt_user.Font = New System.Drawing.Font("標楷體", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.txt_user.Location = New System.Drawing.Point(219, 91)
+        Me.txt_user.Name = "txt_user"
+        Me.txt_user.Size = New System.Drawing.Size(119, 30)
+        Me.txt_user.TabIndex = 0
+        '
+        'txt_pwd
+        '
+        Me.txt_pwd.Font = New System.Drawing.Font("標楷體", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.txt_pwd.Location = New System.Drawing.Point(219, 140)
+        Me.txt_pwd.Name = "txt_pwd"
+        Me.txt_pwd.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txt_pwd.Size = New System.Drawing.Size(119, 30)
+        Me.txt_pwd.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.Button1.Font = New System.Drawing.Font("標楷體", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Button1.Location = New System.Drawing.Point(184, 195)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(113, 40)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "連線"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'TextBox2
+        '
+        Me.TextBox2.BackColor = System.Drawing.SystemColors.Window
+        Me.TextBox2.Font = New System.Drawing.Font("標楷體", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.TextBox2.Location = New System.Drawing.Point(12, 252)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(449, 80)
+        Me.TextBox2.TabIndex = 3
+        '
+        'lbl_user
+        '
+        Me.lbl_user.AutoSize = True
+        Me.lbl_user.Font = New System.Drawing.Font("標楷體", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.lbl_user.Location = New System.Drawing.Point(121, 95)
+        Me.lbl_user.Name = "lbl_user"
+        Me.lbl_user.Size = New System.Drawing.Size(82, 24)
+        Me.lbl_user.TabIndex = 8
+        Me.lbl_user.Text = "帳號："
+        '
+        'lbl_pwd
+        '
+        Me.lbl_pwd.AutoSize = True
+        Me.lbl_pwd.Font = New System.Drawing.Font("標楷體", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.lbl_pwd.Location = New System.Drawing.Point(121, 140)
+        Me.lbl_pwd.Name = "lbl_pwd"
+        Me.lbl_pwd.Size = New System.Drawing.Size(82, 24)
+        Me.lbl_pwd.TabIndex = 9
+        Me.lbl_pwd.Text = "密碼："
+        '
+        'lbl_server
+        '
+        Me.lbl_server.AutoSize = True
+        Me.lbl_server.Font = New System.Drawing.Font("標楷體", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.lbl_server.Location = New System.Drawing.Point(121, 53)
+        Me.lbl_server.Name = "lbl_server"
+        Me.lbl_server.Size = New System.Drawing.Size(82, 24)
+        Me.lbl_server.TabIndex = 6
+        Me.lbl_server.Text = "主機："
+        '
+        'lbl_ip
+        '
+        Me.lbl_ip.AutoSize = True
+        Me.lbl_ip.Font = New System.Drawing.Font("標楷體", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.lbl_ip.Location = New System.Drawing.Point(215, 53)
+        Me.lbl_ip.Name = "lbl_ip"
+        Me.lbl_ip.Size = New System.Drawing.Size(21, 21)
+        Me.lbl_ip.TabIndex = 7
+        Me.lbl_ip.Text = " "
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("新細明體", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Blue
+        Me.Label1.Location = New System.Drawing.Point(208, 338)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(56, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "©sairwolf"
+        '
+        'lbl_title
+        '
+        Me.lbl_title.AutoSize = True
+        Me.lbl_title.Font = New System.Drawing.Font("標楷體", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.lbl_title.ForeColor = System.Drawing.Color.Blue
+        Me.lbl_title.Location = New System.Drawing.Point(90, 9)
+        Me.lbl_title.Name = "lbl_title"
+        Me.lbl_title.Size = New System.Drawing.Size(278, 27)
+        Me.lbl_title.TabIndex = 5
+        Me.lbl_title.Text = "連線網路磁碟機X: Z:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(473, 358)
+        Me.Controls.Add(Me.lbl_title)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lbl_ip)
+        Me.Controls.Add(Me.lbl_server)
+        Me.Controls.Add(Me.lbl_pwd)
+        Me.Controls.Add(Me.lbl_user)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.txt_pwd)
+        Me.Controls.Add(Me.txt_user)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "Net Use XZ"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txt_user As TextBox
+    Friend WithEvents txt_pwd As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents lbl_user As Label
+    Friend WithEvents lbl_pwd As Label
+    Friend WithEvents lbl_server As Label
+    Friend WithEvents lbl_ip As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lbl_title As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,154 @@
+﻿Public Class Form1
+    Public ip As String
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ip = "192.168.0.3"
+        lbl_ip.Text = ip
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim user, pwd
+
+        TextBox2.Text = ""
+        user = txt_user.Text
+        pwd = txt_pwd.Text
+
+        Dim RetVal
+        Dim cmdx
+        cmdx = "net use x: \\" & ip & "\share " & pwd & " /user:" & user
+        RetVal = Shell(cmdx)
+        'MsgBox(cmdx)
+        'RetVal = Shell("calc.exe")
+
+        Dim cmdz
+        cmdz = "net use z: \\" & ip & "\homes " & pwd & " /user:" & user
+        RetVal = Shell(cmdz)
+        'RetVal = Shell("calc.exe")
+
+        'TextBox2 = cmdx & vbCrLf & cmdz
+        System.Threading.Thread.Sleep(2000)
+        Dim cmd, msg
+        cmd = "net use "
+        msg = PMsg(RunCmd(cmd))
+
+        If InStr(msg, "Error") >= 1 Then
+            TextBox2.ForeColor = Color.Red
+            msg = msg & " " & pwd
+        Else
+            TextBox2.ForeColor = Color.Black
+        End If
+
+        TextBox2.Text = msg
+    End Sub
+
+    '此函數來自宜蘭瘋狂魔術師
+    Private Function RunCmd(ByVal command As String) As String
+        '實例一個Process類，啟動一個獨立進程
+        Dim p As Process = New Process()
+
+        'Process類有一個StartInfo屬性，這個是ProcessStartInfo類，包括了一些屬性和方法，下面我們用到了他的幾個屬性：
+
+        p.StartInfo.FileName = "cmd.exe"           '設定程序名
+        p.StartInfo.Arguments = "/c " + command    '設定程式執行參數
+        p.StartInfo.UseShellExecute = False        '關閉Shell的使用
+        p.StartInfo.RedirectStandardInput = True   '重定向標準輸入
+        p.StartInfo.RedirectStandardOutput = True  '重定向標準輸出
+        p.StartInfo.RedirectStandardError = True   '重定向錯誤輸出
+        p.StartInfo.CreateNoWindow = True          '設置不顯示窗口
+
+        p.Start() '啟動
+
+        'p.StandardInput.WriteLine(command);       '也可以用這種方式輸入要執行的命令
+        'p.StandardInput.WriteLine("exit");        '不過要記得加上Exit要不然下一行程式執行的時候會當機
+
+        Return p.StandardOutput.ReadToEnd()        '從輸出流取得命令執行結果
+
+    End Function
+
+    '處理msg 只找出開頭有OK的行
+    '傳入msg
+    '傳回處理好的msg
+    Private Function PMsg(ByVal msg As String) As String
+        'OK X: \\192.168.1.112\share Microsoft Windows Network
+        'OK Z: \\192.168.1.112\homes Microsoft Windows Network
+        '清單沒有任何項目
+        'For Each sLine In arrText
+        'Console.WriteLine(sLine)
+        'Next
+        ' Do While (Not EOF(filenum))
+        'line Input #filenum, tmpstr1
+        'tmpstr = tmpstr & tmpstr1 & "|"
+        'Loop
+        Dim tmpstr, sLine, dOk, reStr
+        tmpstr = Split(msg, vbCrLf)
+        For Each sLine In tmpstr
+            'MsgBox(sLine)
+            'MsgBox(InStr(sLine, "OK"))
+            If InStr(sLine, "OK") >= 1 Then
+                dOk = 1
+                reStr &= good(sLine)
+            End If
+        Next
+
+        If dOk <> 1 Then
+            reStr = "Error!連線不成功!"
+        End If
+
+        Return reStr
+    End Function
+
+    '處理msg net use * /del /y
+    '傳入msg
+    '傳回處理好的msg
+    Private Function PMsgDel(ByVal msg As String) As String
+        '清單沒有任何項目
+        Dim tmpstr, sLine, reStr
+        tmpstr = Split(msg, vbCrLf)
+        For Each sLine In tmpstr
+            'MsgBox(sLine)
+            'MsgBox(InStr(sLine, "OK"))
+            If InStr(sLine, "清單沒有任何項目") >= 1 Then
+                reStr = "Logout！清單沒有任何項目"
+            End If
+        Next
+
+        Return reStr
+    End Function
+
+    '只取要的文字
+    Private Function good(ByVal gLine As String) As String
+        Dim gStr, gTmp
+        gTmp = Split(delSpace(gLine))
+        gStr = gTmp(1) & " " & gTmp(2) & " " & gTmp(3) & vbCrLf
+        Return gStr
+    End Function
+
+    '刪除字串中不必要的空白
+    Private Function delSpace(ByVal mStr As String) As String
+        Dim tmp, reStr
+        tmp = Split(mStr)
+        For Each mWord In tmp
+            If mWord <> "" Then
+                reStr = reStr & " " & mWord
+            End If
+        Next
+        reStr = reStr & vbCrLf
+
+        Return reStr
+    End Function
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+        '清除所有連線
+        Dim cmdd, RetVal
+        cmdd = "net use * /del /y"
+        RetVal = Shell(cmdd)
+        'RetVal = Shell("calc.exe")
+
+        System.Threading.Thread.Sleep(2000)
+        Dim cmd, msg
+        cmd = "net use "
+        msg = PMsgDel(RunCmd(cmd))
+
+        TextBox2.Text = msg
+    End Sub
+End Class
